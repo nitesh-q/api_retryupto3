c60d60705ae5a26cae84edf179f1026766be1028
nitesh <nitesh.gaud@e-stonetech.com>
2023-02-03 12:14:21 +0530

    removed

--- practice.rb
+++
@@ -1,9 +0,0 @@
-
-  require 'httparty'
-  require 'digest'
-  response = HTTParty.get("http://localhost:8888/auth")
-  print(response.headers["badsec-authentication-token"])
-  temp = response.headers["badsec-authentication-token"] + "/users"
-  lol=Digest::SHA256.hexdigest temp
-    response1 = HTTParty.get("http://localhost:8888/users",headers:{"X-Request-Checksum" => lol})
-  print(response1)
